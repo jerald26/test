3882a5b6d5f08dbf1f9cbbb0f6346827fd0d4d11
jerald26 <95662963+jerald26@users.noreply.github.com>
2022-12-03 17:22:32 +0800

    Update CustomerReview.js

--- src/components/content/CustomerReview.js
+++ src/components/content/CustomerReview.js
@@ -11,14 +11,12 @@ const CustomerReview = () => {
       <section className="flex flex-wrap gap-5 justify-center items-center p-10">
         <div className="relative w-[400px] h-[250px] bg-gray-800 flex flex-col items-center rounded-md">
           <p className="text-zinc-200 text-md text-justify px-5 mt-5">
-            <span className="text-pink-600 font-bold text-xl">" </span>Two thumbs up for Sir Rey. great service and great arrangement.
-              kahit sabi nung isa flower shop near SM Calamba na hindi sila
-              kilala but still SUPERB and highly recommended ko sila. last
-              minute nagpakuha sya ng flower sa dangwa just to accommodate me
-              despite of my late notice. God speed po. THE BEST!
+            <span className="text-pink-600 font-bold text-xl">" </span>Highly recommended! They were able to grant all the modifications
+              that I requested, from the flowers to the ribbons. They also
+              delivered the bouquet on time. Thank you Kuya Rey Flower Shop!
             <span className="text-pink-600 font-bold text-xl"> "</span>
           </p>
-          <h3 className="text-pink-600 text-xl font-bold absolute bottom-2 right-5">Jing Florante</h3>
+          <h3 className="text-pink-600 text-xl font-bold absolute bottom-2 right-5">Reina Kariza Sablan Agujar</h3>
         </div>
         <div className="relative w-[400px] h-[250px] bg-gray-800 flex flex-col items-center rounded-md">
           <p className="text-zinc-200 text-md text-justify px-5 mt-5">
